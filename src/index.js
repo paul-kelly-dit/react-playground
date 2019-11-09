03ae02f84c697046efe423bea16969d881859ffb
Paul Kelly <paulkelly@Pauls-MacBook-Pro-2.local>
2019-11-09 14:41:06 +0000

    updated desc

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import { render } from "react-dom";
 const node = document.getElementById("root");
 
 // See we are doing lots of nesting - react is composable - we do this via children.
+
+// here we create stateless react elements
+
 const root = React.createElement(
   "div",
   {},
@@ -14,7 +17,7 @@ const root = React.createElement(
       "a",
       { href: "mailto:paul@tu.ie" },
       React.createElement("h6", {}, "Playing with React"),
-      React.createElement("em", {}, "...")
+      React.createElement("em", {}, "here we create stateless react elements")
     )
   )
 );
